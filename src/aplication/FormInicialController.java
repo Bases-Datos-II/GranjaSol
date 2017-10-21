76d6315e22a2b39a409c0927696985a897dd08cf
Ronaldo Carcamo <carcamoronaldo@gmail.com>
2017-10-21 07:24:35 -0600

    Prueba con Branchs

--- src/aplication/FormInicialController.java
+++ src/aplication/FormInicialController.java
@@ -20,7 +20,7 @@ public class FormInicialController implements Initializable{
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		cmbFamiliaAnimal.getItems().addAll("Bovinos","Aves","Rumiante","Porcino","Ovino","Caprino","Otro");
+		cmbFamiliaAnimal.getItems().addAll("Bovinos","Aves","Rumiante","Porcino","Ovino","Caprino");
 		cmbTipoAnimal.setItems(listaTipoAnimal);
 		cmbEspecieAnimal.setItems(listaEspecieAnimal);
 	}
