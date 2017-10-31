b98a7413a0e22160a72301995260674c87e0fbf5
BPalace <aapp14000@hotmail.es>
2017-10-30 21:35:26 -0600

    Nuevo Formulario, con su respectiva clase

--- src/aplication/FormDietasDiariasController.java
+++ src/aplication/FormDietasDiariasController.java
@@ -1,5 +1,32 @@
 package aplication;
 
-public class FormDietasDiariasController {
+import java.net.URL;
+import java.sql.Date;
+import java.util.ResourceBundle;
+
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import modelo.DietaPorDia;
+import modelo.Historial;
+
+public class FormDietasDiariasController implements Initializable{
+	private ObservableList<DietaPorDia> informacion;
+
+	@FXML private TableView<DietaPorDia> tblInformacion;
+
+	@FXML private TableColumn<DietaPorDia,Number> clmncodigoDiario;
+	@FXML private TableColumn<DietaPorDia,Historial> clmncodigoHistorial;
+	@FXML private TableColumn<DietaPorDia,String> clmncomentarioDiario;
+	@FXML private TableColumn<DietaPorDia,Date> clmnfechaDiario;
+	@FXML private TableColumn<DietaPorDia,String> clmncumplimientoDiario;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
